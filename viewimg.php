38660b66e0d8c5c2483e75a81cadbcea653f2a53
SS-2020 <gsubhasree2@gmail.com>
2020-09-13 21:48:32 +0530

    updated

--- viewimg.php
+++ viewimg.php
@@ -1,9 +1,52 @@
 <?php 
 require_once "config.php";
-$name=$_GET['name'];
-$query="SELECT * FROM items WHERE name='$name'";
-$result= mysqli_query($link,$query);
-$row=mysqli_fetch_assoc( $result);
-header("Content-Type: image/jpeg");
-echo $row['data'];
+$iid=$_GET['iid'];
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<style>
+	body{ 
+		background-color: #663399;
+		}
+	td{
+		padding:10px;
+		border: solid black 2px;
+		position:absolute;
+		left:30%;
+		background-color:white;
+		text-align: center;
+		font-size:20px;
+		font-weight:normal;
+	}
+	</style>
+</head>
+<body>
+<?php
+echo "<table id='gallery'><tr>";
+$query="SELECT * FROM items WHERE id='$iid'";
+$result= mysqli_query($link,$query);
+while($data=mysqli_fetch_assoc( $result))
+{
+	$iid=$data["id"];
+	$type=$data["type"];
+	$name=$data["itemname"];
+	$desc=$data["description"];
+	$quan=$data["quantity"];
+	$price=$data["price"];
+	$desc=$data["description"];
+	$image=$data["image"];
+	echo "<td><a target='_blank' href='viewimg.php?iid=$iid'><img src='".$image."' width='500' height='500'/></a>
+			<br><b>Type:</b>$type
+			<br><b>Name:$name</b>
+			<br><b>Decription:</b>$desc
+			<br>No:$quan
+			<br>Rs.$price
+			</td>";
+}	
+echo "</tr></table>";
+?>
+</body>
+</html>
